1b5a2794a91cccd9adc9dc4065115e8b68d7a2f4
Zhouzw <122282628+Zzw131537@users.noreply.github.com>
2024-12-21 23:06:48 +0800

    Zhouzw

---
+++ src/main/java/ldu/questionbank/student/repository/StudentDao.java
@@ -0,0 +1,28 @@
+package ldu.questionbank.student.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+/**
+* 功能:
+* 作者: Zhouzw
+* 日期: 2024/12/21 21:30
+*/
+@Repository
+
+public class StudentDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate ;
+
+    /**
+     * 根据id 返回题库密码
+     * @param id
+     * @return
+     */
+    public String getPasswordFromBankById(Integer id) {
+        String sql = "select password from bank where id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, String.class);
+    }
+}
---
+++ src/main/java/ldu/questionbank/student/service/StudentService.java
@@ -0,0 +1,29 @@
+package ldu.questionbank.student.service;
+
+import ldu.questionbank.student.repository.StudentDao;
+import org.hibernate.annotations.Array;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 功能: 定义 service 与 dao 层的交互
+ * 作者: Zhouzw
+ * 日期: 2024/12/21 20:47
+ */
+@Service
+public class StudentService {
+    @Autowired
+    private StudentDao studentDao = new StudentDao();
+
+
+    /**
+     * 根据id 返回题库密码
+     * @param id
+     * @return
+     */
+    public String getPasswordFromBankById(Integer id){
+        return studentDao.getPasswordFromBankById(id);
+    }
+
+
+}
