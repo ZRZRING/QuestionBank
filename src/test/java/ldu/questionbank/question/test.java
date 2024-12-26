d8757bbd4098df5c2f084a41e21e21619cb9a6c2
zrzring <zrzring@qq.com>
2024-12-26 14:50:57 +0800

    update .gitignore

---
+++ src/test/java/ldu/questionbank/question/test.java
@@ -0,0 +1,11 @@
+package ldu.questionbank.question;
+
+import ldu.questionbank.entity.Admin;
+
+public class test {
+    public static void main(String[] args) {
+        Admin admin = new Admin();
+        admin.setId(1);
+        System.out.println(admin.getId());
+    }
+}
