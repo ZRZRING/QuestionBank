5480313aa8f535bd67fb2c08f11880e692f838a2
panjy-china <3116898733@qq.com>
2024-12-27 16:33:08 +0800

    添加题目批量导入功能，美化前端页面

--- src/main/java/ldu/questionbank/config/CorsConfig.java
+++ src/main/java/ldu/questionbank/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:63342")
+                        .allowedOrigins("http://localhost:63343")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
